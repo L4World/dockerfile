244dcbe5f9053d4dcbf55ba00c3d71ebe5c3069e
L4World <33340462+L4World@users.noreply.github.com>
2018-10-31 17:41:26 +0800

    Update initdocker.sh

--- initdocker.sh
+++ initdocker.sh
@@ -2,4 +2,6 @@ sudo yum update -y \
 && sudo yum install -y yum-utils device-mapper-persistent-data lvm2 \
 && sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo \
 && sudo yum makecache fast \
-$$ sudo yum install -y docker-ce
+&& sudo yum install -y docker-ce \
+&& sudo systemctl enable docker \
+&& sudo systemctl start docker
